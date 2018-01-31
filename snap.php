e30bdb218c6ca12963064e601a1957e6849717b3
Peter Street <PeterBStreet@gmail.com>
2018-01-31 09:18:32 -0700

    Daily chalenge solution start

--- snap.php
+++ snap.php
@@ -62,3 +62,46 @@ trait Abs {
 				$this->setModel($newModel);
 			}
 
+
+			class Vehicle {
+			protected $plateNo;
+
+			public function __constructor(string $newPlateNo);
+				try {
+				$this->setPlateNo($newPlatNo)
+			catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+			}}
+}
+
+			public function get PlateNo() : String {
+			return($this->plateNo);
+			}
+			public function setPlateNo($newPlateNo) : void {
+			$newPlateNo - filter_var($newPlateNo, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+			$newPlateNo = strptoupper(trim($newPlateNo));
+
+			if(preg_match("/^[A-Z]{3}\d{3}$/", $nwePlateNo) )
+			throw(new InvalidArgumentException("Bad Plate Number - Your a Dolt"));
+				$this->plateNo = $nwPlateNo;
+
+
+				class ParadeFloat extends Vehicle {
+				protected $isGettingAwayVerySlowly;
+
+				public function getIsGettingAwayVerySlowly() : bool {
+				return($this->isGettingAwayVerySowly);
+				}
+				public function setIsGettingAwayVerySowly($newIsGettingAwayVerySowly) : void {
+				$newIsGettingAwayVerySlowly = filter_var($newGettingAwayVerySlowly, FILTER_VALIDATE_BOOLEAN,)
+				if($newIsGettingAwayVerySlowly === null){
+				throw(new InvalidArgumentException("unable to determine if he is getting away"));
+				}
+				$this->isGettingAwayVerySlowly = $newIsGettingAwayVerySlowly;
+}}
+				}
+/*
+0=doesnotmatch
+1=match
+false=error
